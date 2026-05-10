8353f53bd804a97e0f0c55d8c367481fc193a1e1
ANDREW <verifieddeveloper13@gmail.com>
2026-05-10 21:01:36 +0300

    fix: correct vercel wsgi deployment

--- medicinal/wsgi.py
+++ medicinal/wsgi.py
@@ -1,19 +1,14 @@
 """
 WSGI config for medicinal project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
+# Ensure correct settings module is used
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'medicinal.settings')
 
 application = get_wsgi_application()
 
+# Vercel / serverless compatibility alias
 app = application
