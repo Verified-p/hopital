ff4f87187e486f2860ba89d463cd4a00abf0d3df
ANDREW <verifieddeveloper13@gmail.com>
2026-05-19 09:09:00 +0300

    configure postgres database

--- medicinal/settings.py
+++ medicinal/settings.py
@@ -2,6 +2,11 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 
+import dj_database_url
+
+
+
+
 # ================================
 # LOAD ENV VARIABLES
 # ================================
@@ -104,11 +109,15 @@ TEMPLATES = [
 # ================================
 # DATABASE
 # ================================
+
+
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600
+    )
 }
 
 
