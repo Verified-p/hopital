f024eab65c660c32b5db5f4fb07de129987d4410
ANDREW <verifieddeveloper13@gmail.com>
2026-05-19 14:06:43 +0300

    Fixed vercel routing

--- medicinal/wsgi.py
+++ medicinal/wsgi.py
@@ -1,12 +1,12 @@
-"""
-WSGI config for medicinal project.
-"""
-
 import os
+
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'medicinal.settings')
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE',
+    'medicinal.settings'
+)
 
 application = get_wsgi_application()
 
-# Vercel compatibility
+app = application
